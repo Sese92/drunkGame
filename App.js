d502f18992158dd263710907c13245c329ff78dc
Jose <jose.d.j.d@gmail.com>
2020-11-16 11:18:35 +0100

    prettier, eslint & husky configuration

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Git config test</Text>
-      <StatusBar style="auto" />
+      <Text>Git config test!</Text>
     </View>
   );
 }
@@ -14,7 +12,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
