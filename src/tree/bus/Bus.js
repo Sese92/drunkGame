e85c264192c8ba65b73b5a20701f70d5285f9329
Jose <jose.d.j.d@gmail.com>
2020-11-19 16:07:54 +0100

    card flip

--- src/tree/bus/Bus.js
+++ src/tree/bus/Bus.js
@@ -1,43 +1,49 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { SafeAreaView, View, Text } from 'react-native';
+import {
+  // useSelector,
+  useDispatch,
+} from 'react-redux';
+import { SafeAreaView, View } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 
-import { setTurn, removeCard } from '../../services/game/game.service';
 import {
-  selectTurn,
-  selectCard,
-  selectPlayers,
-} from '../../features/gameConfiguration/configuration.store';
-import { Button } from '../../ui/atoms/Button';
+  setTurn,
+  // removeCard
+} from '../../services/game/game.service';
+import // selectTurn,
+// selectCard,
+// selectPlayers,
+'../../features/gameConfiguration/configuration.store';
+// import { Button } from '../../ui/atoms/Button';
+import { Card } from '../../ui/atoms/Card';
 import { flex } from '../../ui/style/layout';
-import { margins } from '../../ui/style/spacing';
 
 export const Bus = () => {
   const { colors } = useTheme();
   const dispatch = useDispatch();
 
-  const card = useSelector(selectCard);
-  const players = useSelector(selectPlayers);
-  const turn = useSelector(selectTurn);
+  // const card = useSelector(selectCard);
+  // const players = useSelector(selectPlayers);
+  // const turn = useSelector(selectTurn);
 
   useEffect(() => {
     dispatch(setTurn({ turn: 0 }));
   }, []);
 
-  function setNextTurn() {
-    dispatch(removeCard({ card: card }));
-    // if (turn < players.length - 1) {
-    //   dispatch(setTurn({ turn: turn + 1 }));
-    // } else {
-    //   dispatch(setTurn({ turn: 0 }));
-    // }
-  }
+  // function setNextTurn() {
+  //   dispatch(removeCard({ card: card }));
+  // if (turn < players.length - 1) {
+  //   dispatch(setTurn({ turn: turn + 1 }));
+  // } else {
+  //   dispatch(setTurn({ turn: 0 }));
+  // }
+  // }
 
   return (
     <SafeAreaView style={[flex.on, { backgroundColor: colors.tertiary }]}>
       <View style={[flex.centerContent]}>
-        <Text
+        <Card card={{ number: 10, color: 'red', type: '♥' }} />
+        {/* <Text
           style={[
             {
               textAlign: 'center',
@@ -52,7 +58,7 @@ export const Bus = () => {
           <Button onPress={() => setNextTurn()}>
             <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Roll it!</Text>
           </Button>
-        </View>
+        </View> */}
       </View>
     </SafeAreaView>
   );
--- src/ui/atoms/Card/Card.js
+++ src/ui/atoms/Card/Card.js
@@ -1,27 +1,123 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import CardFlip from 'react-native-card-flip';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-export const Card = ({ children }) => {
+import { margins } from '../../style/spacing';
+
+export const Card = ({ card }) => {
   const { colors } = useTheme();
+
+  var cardDisplay;
+
+  function RenderCard() {
+    switch (card.number) {
+      case 1:
+        return;
+      case 7:
+        return;
+      case 8:
+        return;
+      default:
+        return (
+          <Text style={{ fontSize: 150, color: card.color }}>
+            {card.number}
+          </Text>
+        );
+    }
+  }
 
   return (
     <View
       style={{
-        backgroundColor: colors.white,
-        width: '80%',
-        height: '60%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 20,
-        shadowColor: colors.black,
-        shadowOffset: {
-          width: 4,
-          height: 4,
-        },
-        shadowOpacity: 1,
+        width: '90%',
+        height: '70%',
       }}>
-      {children}
+      <CardFlip
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        ref={(c) => (cardDisplay = c)}>
+        {/* Front Side */}
+        <TouchableWithoutFeedback
+          style={{
+            backgroundColor: colors.white,
+            borderRadius: 20,
+            borderColor: colors.black,
+            borderWidth: 4,
+          }}
+          onPress={() => cardDisplay.flip()}>
+          <Text
+            style={{
+              position: 'absolute',
+              top: 1,
+              left: 8,
+              fontSize: 30,
+              fontWeight: 'bold',
+              color: card.color,
+            }}>
+            {card.number}
+            {card.type}
+          </Text>
+          <View style={[margins.m9]}>
+            <View
+              style={[
+                {
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: 20,
+                  borderColor: colors.black,
+                  borderWidth: 1,
+                },
+              ]}>
+              {RenderCard()}
+            </View>
+          </View>
+
+          <Text
+            style={{
+              position: 'absolute',
+              fontSize: 30,
+              fontWeight: 'bold',
+              bottom: 1,
+              right: 8,
+              color: card.color,
+            }}>
+            {card.number}
+            {card.type}
+          </Text>
+        </TouchableWithoutFeedback>
+
+        {/* Back Side */}
+        <TouchableWithoutFeedback onPress={() => cardDisplay.flip()}>
+          <View
+            style={{
+              width: '100%',
+              height: '100%',
+              backgroundColor: colors.info,
+              borderRadius: 10,
+              borderWidth: 2,
+              borderColor: colors.black,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 80,
+                fontWeight: 'bold',
+                textTransform: 'uppercase',
+                color: colors.primary,
+              }}>
+              Bus game
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </CardFlip>
     </View>
   );
 };
